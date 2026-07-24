f058c99792c19657ea1b8de71421b4608e69735b
S N KAVYA <nagakavyas@gmail.com>
2026-07-24 10:45:03 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (22.56%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public double myPow(double x, int n) {
        long p = n;
@@ -14,5 +15,24 @@ class Solution {
             p /= 2;
         }
        return ans;
+    }
+}*/
+class Solution {
+    public double myPow(double x, int n) {
+        long p = n;
+        if (p < 0) {
+            x = 1 / x;
+            p = -p;
+        }
+        return power(x, p);
+    }
+    public double power(double x, long n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n == 2) return x * x;
+        if (n % 2 == 0)
+            return power(power(x, n / 2), 2);
+        else
+            return x * power(power(x, n / 2), 2);
     }
 }
