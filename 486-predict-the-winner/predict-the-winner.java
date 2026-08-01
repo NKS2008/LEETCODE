1abe291652d3943a85aecca21acc1ff5d62b517a
S N KAVYA <nagakavyas@gmail.com>
2026-08-01 23:37:03 +0530

    Time: 1 ms (51.87%) | Memory: 43 MB (23.56%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,30 @@
+class Solution {
+    int[][] dp;
+
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        }
+
+        return solve(nums, 0, n - 1) >= 0;
+    }
+
+    private int solve(int[] nums, int i, int j) {
+        if (i == j) {
+            return nums[i];
+        }
+
+        if (dp[i][j] != Integer.MIN_VALUE) {
+            return dp[i][j];
+        }
+
+        int left = nums[i] - solve(nums, i + 1, j);
+        int right = nums[j] - solve(nums, i, j - 1);
+
+        dp[i][j] = Math.max(left, right);
+        return dp[i][j];
+    }
+}
