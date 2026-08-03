3d0d49f311d277f44d27694545358ee33441ff7a
S N KAVYA <nagakavyas@gmail.com>
2026-08-03 09:53:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (17.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,5 +1,6 @@
 class Solution {
     public int climbStairs(int n) {
+        /*
         if(n<= 2){
             return n;
         }
@@ -10,6 +11,18 @@ class Solution {
             c = p1+p2;
             p2 = p1;
             p1 = c;
+        }
+        return c;
+        */
+        if(n <= 2){
+            return n;
+        }
+        int p = 1; 
+        int c = 1;
+        for(int i = 2; i <= n; i++){
+            int t = c;
+            c = p+c;
+            p = t;
         }
         return c;
     }
