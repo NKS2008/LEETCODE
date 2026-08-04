12e6ee9955aad9dbd4a6054c0f666cd9327a9650
S N KAVYA <nagakavyas@gmail.com>
2026-08-04 22:48:46 +0530

    Time: 5 ms (43.94%) | Memory: 46.8 MB (53.61%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
+        /*
         List<Integer> ne = new ArrayList<>();
         int n = nums.length;
         Arrays.sort(nums);
@@ -11,5 +12,21 @@ class Solution {
             }
         }
         return ne;
+        */
+        List<Integer> set = new ArrayList<>();
+        int mi = Integer.MAX_VALUE;
+        int ma = Integer.MIN_VALUE;
+        for(int num : nums){
+            mi = Math.min(mi, num);
+            ma = Math.max(ma, num);
+            set.add(num);
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int i = mi + 1; i < ma ; i++){
+            if(!set.contains(i)){
+                ans.add(i);
+            }
+        }
+        return ans;
     }
 }
